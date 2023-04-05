f2d042af2a888ebea3539f9772887c51b4617e40
mhnaufal <aal.pirates16@gmail.com>
2023-04-05 11:07:14 +0700

    state

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, HttpRequest, Result};
 use actix_files::NamedFile;
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder, Result};
 use std::path::PathBuf;
 
 #[get("/")]
@@ -7,18 +7,26 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("YES")
 }
 
-async fn index(req: HttpRequest) -> impl Responder {
-    req.query_string().to_string()
+struct AppState {
+    app_name: String,
+}
+
+async fn index(req: HttpRequest, data: web::Data<AppState>) -> impl Responder {
+    let app_name = &data.app_name;
+    "App name: ".to_owned() + app_name + " " + &req.query_string().to_string()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix Page"),
+            }))
             .service(hello)
             .service(web::scope("/app").route("index", web::get().to(index)))
     })
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
