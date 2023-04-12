739df1b17e89fef9184d257341f0b0d58067f95e
mhnaufal <aal.pirates16@gmail.com>
2023-04-12 11:03:30 +0700

    it's f works!

--- src/cart.rs
+++ src/cart.rs
@@ -1,10 +1,14 @@
+use std::sync::Mutex;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, PartialEq)]
 pub struct Product {
     pub name: String,
     pub quantity: u64,
 }
 
 pub struct Cart {
-    pub(crate) products: Vec<Product>,
+    pub(crate) products: Mutex<Vec<Product>>,
 }
 
 pub trait ICart {
@@ -17,22 +21,18 @@ impl ICart for Cart {
     fn tambah_produk(&mut self, i_name: String, i_quantity: u64) {
         let new_product: Product = Product { name: i_name, quantity: i_quantity };
 
-        self.products.push(new_product);
+        self.products.lock().unwrap().push(new_product);
     }
 
     fn hapus_produk(&mut self, i_name: String) {
-        self.products.remove(i_name.parse().unwrap());
+        self.products.lock().unwrap().remove(i_name.parse().unwrap());
     }
 
     fn tampilkan_cart(&mut self) -> String {
-        for product in &self.products { // iterate over a copy of self.products
+        for product in self.products.lock().unwrap().iter() { // iterate over a copy of self.products
             println!("{} ({})", product.name, product.quantity);
         };
 
         "List of products".to_string()
-
-        // self.products.iter().map(ToString::to_string).collect()
-
-        // format!("{} ({})", product.name, product.quantity)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,57 @@
+use std::ops::DerefMut;
 use actix_files::NamedFile;
 use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder, Result, delete};
 use std::path::PathBuf;
+use std::sync::Mutex;
 use crate::cart::{Cart, ICart, Product};
 use serde::{Deserialize, Serialize};
 
 mod cart;
 
 #[derive(Deserialize)]
-struct Info {
+struct ReqBodyPayload {
     name: String,
     quantity: u64,
 }
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Homepage will come soon!")
+    HttpResponse::Ok().body("Welcome to Rust Cart API!")
 }
 
 #[get("/cart")]
-async fn show_cart() -> impl Responder {
-    let mut cart1 = Cart { products: Vec::new() };
-
-    cart1.tambah_produk(String::from("Jeruk"), 9);
-    cart1.tambah_produk(String::from("Anggur"), 71);
-    cart1.tampilkan_cart();
-
-    HttpResponse::Ok().body(cart1.tampilkan_cart())
+async fn show_cart(db: web::Data<Cart>) -> impl Responder {
+    HttpResponse::Ok().json(db.products.lock().unwrap().to_vec())
 }
 
 #[post("/cart")]
-async fn add_product(info: web::Json<Info>) -> impl Responder {
-    let _apple = Product { name: String::from("apple"), quantity: 23 };
-
-    let mut cart = Cart { products: Vec::new() };
-
-    cart.tambah_produk(info.name.to_string(), info.quantity);
-
-    HttpResponse::Ok().body("successfully adding a new product")
+async fn add_product(db: web::Data<Cart>, req_payload: web::Json<ReqBodyPayload>) -> impl Responder {
+    let mut added_product = db.products.lock().unwrap();
+    added_product.push(Product { name: req_payload.name.to_string(), quantity: req_payload.quantity });
+    HttpResponse::Ok().json("Successfully adding a new product to cart!")
 }
 
-// #[delete("/cart")]
-// async fn delete_product() -> impl Responder {}
+#[delete("/cart")]
+async fn delete_product(db: web::Data<Cart>, req_payload: web::Json<ReqBodyPayload>) -> impl Responder {
+    let mut deleted_product = db.products.lock().unwrap();
+    *deleted_product = deleted_product.to_vec().into_iter().filter(|x| x.name != req_payload.name.to_string()).collect();
+    HttpResponse::Ok().json("Successfully delete a product!")
+}
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let app_data = web::Data::new(Cart {
+        products: Mutex::new(vec![])
+    });
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(app_data.clone())
             .service(hello)
             .service(show_cart)
             .service(add_product)
+            .service(delete_product)
     })
         .bind(("127.0.0.1", 8080))?
         .run()
